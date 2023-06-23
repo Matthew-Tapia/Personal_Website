39220da364782742c4bb9494319fc0eca247329b
Matthew-Tapia <matthewtapia8@gmail.com>
2023-06-23 12:40:37 -0700

    Added copyright to footer

--- components/Foot.js
+++ components/Foot.js
@@ -1,19 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 const Foot = () => {
   return (
     <div className="flex justify-center">
-      <div className="py-10 space-x-4 text-sky-200">
-        <a
-          target="_blank"
-          href="https://www.linkedin.com/in/matthew-tapia-1b765a1b7/"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-        <a target="_blank" href="https://github.com/Matthew-Tapia">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
+      <div className="py-10 text-center text-sky-200">
+        <div className="space-x-4">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/matthew-tapia-1b765a1b7/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a target="_blank" href="https://github.com/Matthew-Tapia">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+
+        <div className="mt-2 text-xs">Created with Next.js</div>
+
+        <div className="mt-2 text-xs">
+          <FontAwesomeIcon icon={faCopyright} /> 2023 Matthew Tapia
+        </div>
       </div>
     </div>
   );
