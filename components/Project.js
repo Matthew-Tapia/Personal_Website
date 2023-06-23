dbef952a7242d72ab25d66fc324f64469c8e6ac2
Matthew-Tapia <matthewtapia8@gmail.com>
2023-06-23 13:39:40 -0700

    changed gradient stops for project images

--- components/Project.js
+++ components/Project.js
@@ -7,7 +7,7 @@ const Project = ({ url, img, text }) => {
       <div className="grid lg:grid-cols-8 lg:grid-rows-1 grid-rows-8">
         <div className="lg:col-span-3 md:px-14 row-span-3">
           <a target="_blank" href={url}>
-            <div className="rounded-lg border border-amber-200 bg-gradient-to-br from-neutral-400 from-30% to-amber-600">
+            <div className="rounded-lg border border-amber-200 bg-gradient-to-br from-neutral-500 from-20% to-amber-500">
               <Image
                 className="mix-blend-overlay rounded-lg drop-shadow-2xl"
                 height={500}
